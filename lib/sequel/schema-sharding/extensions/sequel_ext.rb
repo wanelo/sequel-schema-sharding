a4867c71013404cf0ef94f50a77e268390e9ae10
Andre Arko & Eric Saxby <pair+andre+sax@wanelo.com>
2014-01-22 14:30:09 -0800

    we don't know what this did
    but deleting it leaves all the tests here and in dependent services
    green!

--- lib/sequel/schema-sharding/extensions/sequel_ext.rb
+++ lib/sequel/schema-sharding/extensions/sequel_ext.rb
@@ -1,16 +1,1 @@
-module Sequel
-  module SchemaSharding
-    module Extensions
-      module SequelExt
-        module ClassMethods
-          def db
-            return @db if @db
-          end
-        end
-      end
-    end
-  end
-end
-
-Sequel::Model.plugin Sequel::SchemaSharding::Extensions::SequelExt
 Sequel::Model.plugin :validation_helpers
