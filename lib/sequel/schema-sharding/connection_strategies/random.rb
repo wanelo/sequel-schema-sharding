ac72487c6dc459752ccfe2cb6ef08e6388d1972e
Eric Saxby & James Hart <sax+james@wanelo.com>
2014-06-19 16:53:24 -0700

    Simplify ruby code by using sample instead of rand

--- lib/sequel/schema-sharding/connection_strategies/random.rb
+++ lib/sequel/schema-sharding/connection_strategies/random.rb
@@ -1,10 +1,14 @@
+# This strategy is used for choosing a :read_only server
+# to connect to.
+#
+# A random server will be chosen from the replica list
+# for this physical shard.
 module Sequel
   module SchemaSharding
     module ConnectionStrategy
       class Random
         def self.choose(db, config)
-          choice = rand(config['replicas'].size)
-          config['replicas'][choice]
+          config['replicas'].sample
         end
       end
     end
