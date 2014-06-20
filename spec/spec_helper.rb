54dc3e5d6223a1e7c0344b0d7cc60461a07e1716
Eric Saxby & James Hart <sax+james@wanelo.com>
2014-06-19 17:05:14 -0700

    Remove deprecation warnings

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,7 +8,6 @@ require 'support/database_helper'
 require 'mocha/api'
 
 RSpec.configure do |config|
-  config.treat_symbols_as_metadata_keys_with_true_values = true
   config.run_all_when_everything_filtered = true
   config.filter_run :focus
   config.alias_example_to :fit, focus: true
